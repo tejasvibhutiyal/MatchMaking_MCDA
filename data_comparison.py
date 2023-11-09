3b021ac8c7cb16b181774308b8e48a89fbe9dd82
tejasvibhutiyal <tbhutiyal@gmail.com>
2023-11-09 10:45:49 -0400

    added docstring and comments

--- data_comparison.py
+++ data_comparison.py
@@ -2,6 +2,18 @@ from base_model import MatchMakingModel
 from scipy.spatial import distance
 
 def compare_data(old_attendees_map, modified_attendees_map, distance_metric = 'cosine'):
+    """
+    Compare embeddings between old and modified attendee data using a specified distance metric.
+
+    Parameters:
+    - old_attendees_map (dict): Dictionary containing names as keys and paragraphs as values for the old data.
+    - modified_attendees_map (dict): Dictionary containing names as keys and paragraphs as values for the modified data.
+    - distance_metric (str, optional): The distance metric to use for comparison. Default is 'cosine'.
+                                     Possible values: 'cosine', 'euclidean', and other valid distance metrics.
+
+    Returns:
+    - comparison_results (dict): Dictionary containing names as keys and similarity scores as values.
+    """
     model = MatchMakingModel()
     old_attendees_embeddings=model.generate_embeddings(old_attendees_map)
     modified_attendees_embeddings = model.generate_embeddings(modified_attendees_map)
@@ -16,7 +28,7 @@ def compare_data(old_attendees_map, modified_attendees_map, distance_metric = 'c
             similarity = 1 - distance.cosine(old_embedding, modified_embedding)
         elif distance_metric == 'euclidean':
             similarity = -distance.euclidean(old_embedding, modified_embedding)
-            # Note: Using negative Euclidean distance as a similarity measure
+            # Using negative Euclidean distance as a similarity measure
 
         # Store the result
         comparison_results[name] = similarity
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from base_model import MatchMakingModel
 import data_comparison
 
-# for 2nd question
+# For 2nd question
 
+# Define the file name and Instantiating the MatchMakingModel
 file_name= 'MCDA5511_classmates_Sheet1.csv'
 model=MatchMakingModel()
 attendees_map= model.csv_to_dict(file_name)
 
+# Define a selected list of classmates for data comparison and modifying their description and storing in dictionary
 selected_list = [
     "Rakshit Gupta",
     "Neeyati Mehta",
@@ -19,12 +21,15 @@ classmates_map_data_change["Neeyati Mehta"] = "I like napping."
 classmates_map_data_change["Sylvester Terdoo"] = "i enjoy spending time inside"
 classmates_map_data_change["Tejasvi Bhutiyal"] = "I love to watch netflix series and discover new places."
 
+# Create a dictionary to store earlier data for selected classmates
 classmates_map_earlier_data = {}
 for name in selected_list:
     classmates_map_earlier_data[name] = attendees_map[name]
 
-print(classmates_map_earlier_data)
+# Compare the modified data using cosine and Euclidean distance metrics
 cosine_similarities= data_comparison.compare_data(classmates_map_earlier_data, classmates_map_data_change)
 eucledian_similarities= data_comparison.compare_data(classmates_map_earlier_data, classmates_map_data_change, 'euclidean')
+
+# Print comparison result
 print('Cosine similarities between old and modifies data:',cosine_similarities)
 print('Eucledian similarities between old and modifies data:',eucledian_similarities)
